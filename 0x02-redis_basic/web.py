ba2aa37aff7767c2db301af04c131d96df39b601
samfass <sfasil374@gmail.com>
2023-05-11 23:22:10 -0700

    commit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
-""" Redis with requests """
+"""
+Web file
+"""
 import requests
-from functools import wraps
-from typing import Callable
 import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
-def count(method: Callable):
-    """ Count the call to requests """
-    r = redis.Redis()
-
+def count_url_access(method):
+    """ Decorator counting how many times
+    a Url is accessed """
     @wraps(method)
-    def wrapped(url):
-        """ function that will count """
-        r.incr(f"count:{url}")
-        expiration_count = r.get(f"cached:{url}")
-        if expiration_count:
-            return expiration_count.decode('utf-8')
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
         html = method(url)
-        r.setex(f"cached:{url}", 10, html)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
         return html
+    return wrapper
 
-    return wrapped
 
-
-@count
+@count_url_access
 def get_page(url: str) -> str:
-    """ module to obtain the HTML """
-    return requests.get(url).text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
